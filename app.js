2d55440888cbee0d18fe628144b3c53831dce0eb
alexis duru <alexis.duru.pro@gmail.com>
2021-09-07 19:03:41 +0200

    projet terminé

--- app.js
+++ app.js
@@ -1,34 +1,56 @@
-let total = 0;
+// let total = 0;
 
-function addition(x) {
-    total += x;
-    return total;
+// function addition(x) {
+//     total += x;
+//     return total;
+// }
+
+// function soustraction(x) {
+//     total -= x;
+//     return total;
+// }
+
+// function division(x) {
+//     if (total === 0) {
+//         return (total = x);
+//     } else {
+//         total /= x;
+//     return total;
+//     }
+// }
+
+// function multiplication(x) {
+//     if (total === 0) {
+//         return (total = x);
+//     } else {
+//         total *= x;
+//     return total;
+//     }
+// }
+
+// function reset() {
+//     total = 0;
+// }
+let input="";
+
+function showValue(btn){
+    input = input + btn.value;
+    console.log(input);
+    document.getElementById('output').innerHTML = input;    
 }
 
-function soustraction(x) {
-    total -= x;
-    return total;
+function quickMath(){
+    document.getElementById('output').innerHTML = eval(input);
 }
 
-function division(x) {
-    if (total === 0) {
-        return (total = x);
-    } else {
-        total /= x;
-    return total;
-    }
+function Reset(btn){
+    document.getElementById('output').innerHTML = "0"
+    input = "";
+} 
+
+function remove(btn){
+    input = input.replace(input.slice(-1), "");
+    document.getElementById('output').innerHTML = input;
 }
 
-function multiplication(x) {
-    if (total === 0) {
-        return (total = x);
-    } else {
-        total *= x;
-    return total;
-    }
-}
-
-function reset() {
-    total = 0;
-}
 
